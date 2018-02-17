e5c5ed91917dc62b53fb9578c8aa13a4beea1f78
hmlb81 <hmlb81@163.com>
2018-02-17 20:12:50 +0800

    Adjust files

--- loggingAuxiliary/__init__.py
+++ loggingAuxiliary/__init__.py
@@ -1,1 +1,9 @@
-print("loggingAuxiliary loaded")
+#Logging module configger
+class loggingConfigger : 
+    #config logging module
+    def config(self) :
+        print("loggingConfigger config called.")
+
+
+configger = loggingConfigger()
+configger.config()
--- loggingAuxiliary/loggingConfigger.py
+++
@@ -1,3 +0,0 @@
-class loggingConfigger : pass
-
-instance = loggingConfigger()
