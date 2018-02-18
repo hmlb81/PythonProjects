4952135cbb85b6ff5aa086bd682942da9d811335
hmlb81 <hmlb81@163.com>
2018-02-18 10:11:40 +0800

    Add some code to loggingConfigger
    Complete later

--- loggingAuxiliary/loggingConfigger.py
+++ loggingAuxiliary/loggingConfigger.py
@@ -5,16 +5,24 @@ import logging.config
 class loggingConfigger : 
     #config logging module to console
     def configConsoleLogging(self) :
-        consoleHandlerClass = logging.StreamHandler
-        consoleHandlerConfig = {}
+        consoleHandlerClass = "logging.StreamHandler"
+        consoleFormatter = "brief"
+        consoleLevel = logging.INFO
+        consoleStream = "ext://sys.stdout"
+        consoleHandlerConfig = { 
+            "class" : consoleHandlerClass,
+            "formatter" : consoleFormatter,
+            #"level" : consoleLevel,
+            #"stream" : consoleStream
+        }
         handlersConfig = { "console" : consoleHandlerConfig }
         config = {
             "version" : 1,
             "handlers" : handlersConfig
         }
-        print(config)
+        print(config) #TODO:debug code, removing later 
         logging.config.dictConfig(config)
-        print("loggingConfigger configConsoleLogging called.")
+        print("loggingConfigger configConsoleLogging called.") #TODO: debug code removing later
 
 
 instance = loggingConfigger()
