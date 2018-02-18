9f48b6b8e616dbe8ee57206b228edeaf31063a48
hmlb81 <hmlb81@163.com>
2018-02-18 10:03:04 +0800

    Add some logging code
    Unfinished, complete later.

--- loggingAuxiliary/loggingConfigger.py
+++ loggingAuxiliary/loggingConfigger.py
@@ -1,7 +1,19 @@
+import logging
+import logging.config
+
 #Logging module configger
 class loggingConfigger : 
     #config logging module to console
     def configConsoleLogging(self) :
+        consoleHandlerClass = logging.StreamHandler
+        consoleHandlerConfig = {}
+        handlersConfig = { "console" : consoleHandlerConfig }
+        config = {
+            "version" : 1,
+            "handlers" : handlersConfig
+        }
+        print(config)
+        logging.config.dictConfig(config)
         print("loggingConfigger configConsoleLogging called.")
 
 
