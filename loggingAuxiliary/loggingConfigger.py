a179d6cdb8ec88eea5e746c4c0854118a2fb3753
hmlb81 <hmlb81@163.com>
2018-02-18 16:23:23 +0800

    Adjust logging config, remove optional formatter.
    To run config.

--- loggingAuxiliary/loggingConfigger.py
+++ loggingAuxiliary/loggingConfigger.py
@@ -6,23 +6,21 @@ class loggingConfigger :
     #config logging module to console
     def configConsoleLogging(self) :
         consoleHandlerClass = "logging.StreamHandler"
-        consoleFormatter = "brief"
         consoleLevel = logging.INFO
         consoleStream = "ext://sys.stdout"
         consoleHandlerConfig = { 
             "class" : consoleHandlerClass,
-            "formatter" : consoleFormatter,
-            #"level" : consoleLevel,
-            #"stream" : consoleStream
+            "level" : consoleLevel,
+            "stream" : consoleStream
         }
         handlersConfig = { "console" : consoleHandlerConfig }
         config = {
             "version" : 1,
             "handlers" : handlersConfig
         }
-        print(config) #TODO:debug code, removing later 
+        
         logging.config.dictConfig(config)
-        print("loggingConfigger configConsoleLogging called.") #TODO: debug code removing later
-
+        root = logging.root #TODO: debugging code,removing later
+        logging.info("loggingConfigger.configConsoleLogging:finished.")
 
 instance = loggingConfigger()
