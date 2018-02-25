f13b0cce46a2bbb5550cd0ed6f9f6df71e975ac4
hmlb81 <hmlb81@163.com>
2018-02-25 22:46:10 +0800

    Add framework codes

--- troubleShooter/program.py
+++ troubleShooter/program.py
@@ -1,8 +1,22 @@
 
+#set import paths. executed before main methods
+import sys
+sys.path.append("..")
+
+import loggingAuxiliary
+import loggingAuxiliary.loggingConfigger
+
+
+
 #main program class
 class program : 
     #main method
-    def run(self) : pass
+    def run(self) : 
+        self._initializeInfrustructures()
+    
+    #initialize infrustructures
+    def _initializeInfrustructures(self) : 
+        loggingAuxiliary.loggingConfigger.instance.configConsoleLogging()
 
 #run program
 _instance = program()
