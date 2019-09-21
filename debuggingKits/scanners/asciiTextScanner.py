f081d04e90b3863a97ff0cffd843740398a62cd4
hmlb81 <hmlb81@163.com>
2019-09-21 21:21:22 +0800

    add some scanner codes
    add some scanner codes.

--- debuggingKits/scanners/asciiTextScanner.py
+++ debuggingKits/scanners/asciiTextScanner.py
@@ -8,7 +8,9 @@ import enum
 # SingleAsciiText (meet ascii char) -> SingleAsciiText
 # SingleAsciiText (meet non ascii char) -> NONE (text not end with '\0')
 # Ucs2TextExpectingAsciiChar (meet ascii char) -> Ucs2TextExpectingNullChar
-# Ucs2TextExpectingAsciiChar (meet '\0') -> NONE
+# Ucs2TextExpectingAsciiChar (meet '\0') -> Ucs2TextExpectingEndingNullByte
+# Ucs2TextExpectingEndingNullByte (meet '\0') -> NONE
+# Ucs2TextExpectingEndingNullByte (meet not '\0') -> NONE
 # Ucs2TextExpectingNullChar (meet '\0') -> Ucs2TextExpectingAsciiChar
 # Ucs2TextExpectingNullChar (meet not '\0') -> NONE
 class asciiTextScannerScanningState : 
@@ -17,8 +19,7 @@ class asciiTextScannerScanningState :
     SingleAsciiText = 2
     Ucs2TextExpectingAsciiChar = 3
     Ucs2TextExpectingNullChar = 4
-
-import ctypes
+    Ucs2TextExpectingEndingNullByte = 5
 
 class asciiTextScanner : 
     def __init__(self) :
@@ -43,6 +44,8 @@ class asciiTextScanner :
             self._scanWhenSingleAsciiText(byte, userState)
         elif (self._state == asciiTextScannerScanningState.Ucs2TextExpectingAsciiChar) :
             self._scanWhenUcs2TextExpectingAsciiChar(byte, userState)
+        elif (self._state == asciiTextScannerScanningState.Ucs2TextExpectingEndingNullByte) :
+            self._scanWhenUcs2TextExpectionEndingNullByte(byte, userState)
         else :
             raise AssertionError("Unsupported scanning state")
     
@@ -57,7 +60,7 @@ class asciiTextScanner :
             self._state = asciiTextScannerScanningState.UncertainText
             return
         else :
-            raise AssertionError("todo:implement")
+            return
     
     def _scanWhenUncertainText(self, byte, userState) :
         isNullChar = self._isNullChar(byte)
@@ -69,16 +72,23 @@ class asciiTextScanner :
         elif isAsciiChar :
             self._processingBytes.append(byte)
             self._state = asciiTextScannerScanningState.SingleAsciiText
+            return
         else :
-            raise AssertionError("todo:implement")
+            #single ascii byte, just treat as noisy
+            self._resetScanningState()
+            return
         
     def _scanWhenSingleAsciiText(self, byte, userState) :
         isNullChar = self._isNullChar(byte)
         isAsciiChar = self._isAsciiChar(byte)
         if isNullChar :
-            raise AssertionError("todo:implement")
+            self._processingBytes.append(byte)
+            self._raiseTextFound(True, userState)
+            self._resetScanningState()
+            return
         elif isAsciiChar :
-            raise AssertionError("todo:implement")
+            self._processingBytes.append(byte)
+            return
         else :
             #text not end with '\0', do not append end byte
             self._raiseTextFound(True, userState)
@@ -86,7 +96,26 @@ class asciiTextScanner :
             return
     
     def _scanWhenUcs2TextExpectingAsciiChar(self, byte, userState) :
-        raise AssertionError("todo:implement")
+        isNullChar = self._isNullChar(byte)
+        if isNullChar :
+            self._processingBytes.append(byte)
+            self._state = asciiTextScannerScanningState.Ucs2TextExpectingEndingNullByte
+            return
+        else :
+            #text not end with '\0', do not append end byte
+            self._raiseTextFound(False, userState)
+            self._resetScanningState()
+            return
+    
+    def _scanWhenUcs2TextExpectionEndingNullByte(self, byte, userState) :
+        isNullChar = self._isNullChar(byte)
+        if isNullChar :
+            self._processingBytes.append(byte)
+            self._raiseTextFound(False, userState)
+            self._resetScanningState()
+            return
+        else :
+            raise AssertionError("todo:implement")
     
     def _raiseTextFound(self, isSingleAsciiText, userState) :
         processingBytesLength = len(self._processingBytes)
@@ -95,10 +124,11 @@ class asciiTextScanner :
         endsWithNullChar = False
         if isSingleAsciiText :
             processingText = self._processingBytes.decode("ascii")
-            processingTextLength = processingBytesLength
-            endsWithNullChar = processingText.endswith("\0")
         else :
-            raise AssertionError("todo:implement")
+            processingText = self._processingBytes.decode("utf_16_le")
+        
+        processingTextLength = processingBytesLength
+        endsWithNullChar = processingText.endswith("\0")        
         
         #try remove noisy notifications
         validProcessingTextLength = processingTextLength
