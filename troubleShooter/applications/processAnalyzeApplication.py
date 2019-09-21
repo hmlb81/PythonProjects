678680f516284baf74121ac84d650cf3953eae95
hmlb81 <hmlb81@163.com>
2019-09-21 15:40:50 +0800

    add some framework codes
    add some framework codes

--- troubleShooter/applications/processAnalyzeApplication.py
+++ troubleShooter/applications/processAnalyzeApplication.py
@@ -3,6 +3,21 @@ from windowsApiWrapper.kernel32Dll import *
 from windowsApiWrapper.processAccess.processHelper import *
 from windowsApiWrapper.processAccess.toolhelp32SnapshotHandle import *
 
+class analyzingProcess :
+    def __init__(self, processID) :
+        self._processID = processID
+        
+    def detectAndFillProcessName(self, thModule) :
+        raise AssertionError("todo:implement")
+
+class analyzingProcessModule : 
+    @staticmethod
+    def prepareAnalyzingProcessModule(analyzingProcess, thModule, analyzingModules) :
+        raise AssertionError("todo:implement")
+    
+    def __init__(self) :
+        pass #todo:implement
+
 class processAnalyzeApplication : 
     @staticmethod
     def getInstance() :
@@ -16,9 +31,17 @@ class processAnalyzeApplication :
             toolhelp32CreateSnapshotFlag.TH32CS_SNAPMODULE32,
         ]
         
+        #walk to generate analyzing modules
+        analyzingModules = []
         for processId in processIds :
+            currentAnalyzingProcess = analyzingProcess(processId)
             snapshotHandle = toolhelp32SnapshotHandle.create(createSnapshotFlags, processId)
-            modules = snapshotHandle.walkModuleEntry()
+            thmodules = snapshotHandle.walkModuleEntry()
+            for thmodule in thmodules :
+                currentAnalyzingProcess.detectAndFillProcessName(thmodule)
+                analyzingProcessModule.prepareAnalyzingProcessModule(processId, thmodule, analyzingModules)
+                raise AssertionError("todo:implement")
+            
             raise AssertionError("todo:implement")
 
 _instance = processAnalyzeApplication()
