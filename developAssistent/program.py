40855a8384a0e9b9f9fb00b6b56b5039bc225c37
hmlb81 <hmlb81@163.com>
2020-02-04 18:26:54 +0800

    add pylint config file
    add pylint config file

--- developAssistent/program.py
+++ developAssistent/program.py
@@ -3,12 +3,13 @@ import os
 #append top level module directory
 os.sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../"))
 
+from applications.dotnetCoreDevelopApplication import dotnetCoreDevelopApplication
 from programFrameworks.simpleProgramFramework import simpleProgramBase
 
 #program entry
 class program(simpleProgramBase) : 
     def _dorun(self) :
-        pass
+        dotnetCoreDevelopApplication.getInstance().showDotnetCoreHelp()
 
 _program = program()
 _program.run()
